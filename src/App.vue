c26206897555cad6330b5c4483e4e1a119bd0306
Nam Nguyen <nam.nguyen.node@gmail.com>
2023-10-22 14:37:04 -0300

    Go 🚀

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,33 @@
-<script setup></script>
+<script setup>
+import { ref, watchEffect } from "vue";
+
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+const first = ref(0);
+const second = ref(0);
+
+watchEffect(async () => {
+  console.log("first run", first);
+  // await sleep(1000);
+
+  // second.value = first.value;
+  // first.value = second.value;
+});
+
+function run() {
+  first.value++;
+}
+</script>
 
 <template>
-  <h1>Hi there</h1>
+  <div>
+    <h1>Hi there</h1>
+    <button @click="run">Click me</button>
+    <p>First: {{ first }}</p>
+    <p>Second: {{ second }}</p>
+  </div>
 </template>
 
 <style scoped></style>
