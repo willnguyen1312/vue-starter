44efd02dd85e9c69cbb42a26d9eb38b62f11a315
Nam Nguyen <nam.nguyen.node@gmail.com>
2023-11-02 11:45:20 -0300

    Otoke

---
+++ src/Child.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+const props = defineProps<{
+  input: number;
+}>();
+
+const emit = defineEmits<{
+  changeStuff: [id: number];
+}>();
+</script>
---
+++ src/Parent.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import Child from "./Child.vue";
+
+type Props = InstanceType<typeof Child>["$props"];
+</script>
+
+<template>
+  <Child :input="123" />
+</template>
